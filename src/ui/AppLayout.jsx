d903030e9d0ee6acbdf0dadc32e49d7e01f7d0df
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2025-02-12 16:26:27 -0500

    Add Header and Sidebar components with styled layout.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,32 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
 import GlobalStyles from "../styles/GlobalStyles";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template: auto 1fr / 26rem 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <>
+    <StyledAppLayout>
       <GlobalStyles />
-      <main>
+      <Header />
+      <Sidebar />
+      <Main>
         <Outlet />
-      </main>
-    </>
+      </Main>
+    </StyledAppLayout>
   );
 }
 
