f399d2dff65ea65da23e38503edea5f2b552996d
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2025-02-12 16:03:03 -0500

    Implement routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import GlobalStyles from "./styles/GlobalStyles";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Rooms from "./pages/Rooms";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import AppLayout from "./ui/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Navigate to="/dashboard" replace /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/bookings", element: <Bookings /> },
+      { path: "/rooms", element: <Rooms /> },
+      { path: "/users", element: <Users /> },
+      { path: "/settings", element: <Settings /> },
+      { path: "/account", element: <Account /> },
+      { path: "/login", element: <Login /> },
+      { path: "*", element: <PageNotFound /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <h1>Hotel Management</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
