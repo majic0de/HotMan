4e4c1162752b25248036a0483cf70b62fee710dc
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2025-02-14 15:05:38 -0500

    Add CreateRoomForm component for room creation.

---
+++ src/features/rooms/CreateRoomForm.jsx
@@ -0,0 +1,113 @@
+import { Form } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+import { useForm } from "react-hook-form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Textarea from "../../ui/Textarea";
+import FileInput from "../../ui/FileInput";
+import Button from "../../ui/Button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createRoom } from "../../services/apiRooms";
+
+const CreateRoomForm = () => {
+  const { register, handleSubmit, errors, getValues, reset } = useForm();
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending: isCreating } = useMutation({
+    mutationFn: createRoom,
+    onSuccess: () => {
+      toast.success("Room created successfully");
+      queryClient.invalidateQueries({
+        queryKey: ["rooms"],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  function onSubmit(data) {
+    mutate(data);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Room name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          {...register("name", {
+            required: "Room name is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapicity"
+          {...register("maxCapicity", {
+            required: "Maximum capacity is required",
+            min: {
+              value: 1,
+              message: "Minimum capacity is 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register("regularPrice", {
+            required: "Regular price is required",
+            min: { value: 1, message: "Minimum price is 1" },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" error={errors?.Discount?.message}>
+        <Input
+          type="text"
+          id="discount"
+          {...register("Discount", {
+            required: "Discount is required",
+            validate: (value) => {
+              Number(value) >= Number(getValues("regularPrice")) ||
+                "Discount must be less than regular price";
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Description for website"
+        error={errors?.Description?.message}
+      >
+        <Textarea
+          id="description"
+          defaulValue=""
+          {...register("Description", {
+            required: "Description is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Room photo" error={errors?.Discount?.message}>
+        <FileInput id="image" accept="image/*" />
+      </FormRow>
+      <FormRow>
+        <Button type="reset" variation="secondary">
+          Cancel
+        </Button>
+        <Button disabled={isCreating} variation="primary" type="submit">
+          {isCreating ? "Is creating" : "Add room"}
+        </Button>
+      </FormRow>
+    </Form>
+  );
+};
+
+export default CreateRoomForm;
