ad15f6298573851fb766400be55d0258268fab14
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2025-02-14 15:06:12 -0500

    Add React Query for data fetching and integrate QueryClientProvider in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {
   RouterProvider,
   Navigate,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -31,8 +33,21 @@ const router = createBrowserRouter([
   { path: "*", element: <PageNotFound /> },
 ]);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
